66590857d52d8cee7ff8fe572dcfb4c7632328bf
Dina Amr <58145677+dinaamrr@users.noreply.github.com>
2021-09-07 01:22:38 +0200

    Update user_info_driver.py

--- exercise_one/scripts/user_info_driver.py
+++ exercise_one/scripts/user_info_driver.py
@@ -1,30 +1,19 @@
 
-#!/usr/bin/python
 import rospy
-from exercise_one.msg import raw_data
-
-
+from std_msgs.msg import String
 def personalData():
-    #Creating a Node
-    rospy.init_node("data_publisher_node",anonymous=True)
-    #Creating a publisher topic
-    pub=rospy.Publisher("data_publisher_topic", raw_data,queue_size=10)
-    rate=rospy.Rate(1) #1Hz
-
-    my_data=raw_data()
-
-    my_data.name="Rose"
-    my_data.age=20
-    my_data.height=170
+    pub = rospy.Publisher('raw_data', String, queue_size=10)
+    rospy.init_node('data_publisher_node', anonymous=True)
+    rate = rospy.Rate(1) # 1hz
 
     while not rospy.is_shutdown():
-       
-        rospy.loginfo("Name: %s,age: %d ,height: %d", my_data.name,my_data.age,my_data.height)
-        pub.publish(my_data)
+        pers_str = "name:Rose, age:20, height:170"
+        rospy.loginfo(pers_str)
+        pub.publish(pers_str)
         rate.sleep()
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     try:
         personalData()
     except rospy.ROSInterruptException:
