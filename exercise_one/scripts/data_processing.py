7fb720e1b3990f170a50aae32ecf55b4a7373163
Dina Amr <58145677+dinaamrr@users.noreply.github.com>
2021-09-07 01:19:50 +0200

    Update data_processing.py

--- exercise_one/scripts/data_processing.py
+++ exercise_one/scripts/data_processing.py
@@ -1,15 +1,20 @@
 
 import rospy
-from exercise_one.msg import raw_data
+from std_msgs.msg import String
 
 def response_to_pub(data):
-    rospy.loginfo("Detail Info:\nName: %s\nage: %d\nheight: %d",data.name,data.age,data.height)
+   name = data.data.split(",")
+   print(name)
+   age = data.data.split(",")
+   print(age)
+   height = data.data.split(",")
+   print(height)
 
 
 
 def personalInfo_pub():
-    rospy.init_node("data_subscriber_node",anonymous=True)
-    sub=rospy.Subscriber("data_publisher_topic",raw_data,response_to_pub)
+    rospy.init_node('data_subscriber_node',anonymous=True)
+    sub=rospy.Subscriber("raw_data",String,response_to_pub)
     rospy.spin()
 
 
